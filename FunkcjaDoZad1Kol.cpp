3b179f068ada873efff20e6005b90e1b3a1ea873
JacKoz7 <126762316+JacKoz7@users.noreply.github.com>
2023-06-14 12:43:32 +0200

    Create FunkcjaDoZad1Kol.cpp

---
+++ FunkcjaDoZad1Kol.cpp
@@ -0,0 +1,21 @@
+int func(kolejka *k)
+{
+    int nieparzyste = 0;
+    kolejka *k_kopia;
+    
+    while(isEmpty(k) != 1)
+    {
+        int obecna_wartosc = first(k);
+        push(k_kopia, pop(k));
+        
+        if (obecna_wartosc % 3 == 0)
+        {
+            nieparzyste++;
+        }
+    }
+    while(isEmpty(k_kopia) != 1)
+    {
+        push(k, pop(k_kopia));
+    }
+    return nieparzyste;
+}
